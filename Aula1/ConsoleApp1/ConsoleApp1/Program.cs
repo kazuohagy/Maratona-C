61b4bcac74242ea622ee8e57adf566aabe571e76
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-25 12:33:24 -0300

    commit :D

--- Aula1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Aula1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,8 +2,17 @@
 Console.WriteLine("Hello Worldd!");
 
 string name = "Mario";
-string sobrenome = "Silvia";
+string sobrenome = "Bros";
 
 Console.WriteLine("Hello " + name + " " + sobrenome);
 Console.WriteLine($"{name} {sobrenome}");
 
+int valor1 = 10;
+int valor2 = 20;
+
+Console.WriteLine("A soma dos valores e " +(valor1 + valor2));
+
+decimal valor3 = 10.5m;
+decimal valor4 = 20.5m;
+
+Console.WriteLine($"A soma dos decimais e: {valor3 + valor4}");
