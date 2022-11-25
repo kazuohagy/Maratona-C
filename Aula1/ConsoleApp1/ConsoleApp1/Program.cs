bff9569d2db7a1c04cdbb3ce97fae59eb53f1899
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-24 22:54:06 -0300

    initial

---
+++ Aula1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Worldd!");
+
+string name = "Mario";
+string sobrenome = "Silvia";
+
+Console.WriteLine("Hello " + name + " " + sobrenome);
+Console.WriteLine($"{name} {sobrenome}");
+
