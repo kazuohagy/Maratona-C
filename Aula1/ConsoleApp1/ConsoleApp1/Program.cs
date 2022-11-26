9de509d48e1417838f9b26c42b2ddf699f2937eb
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-26 19:55:48 -0300

    testCommit :rocket:

--- Aula1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Aula1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,5 +14,41 @@ Console.WriteLine("A soma dos valores e " +(valor1 + valor2));
 
 decimal valor3 = 10.5m;
 decimal valor4 = 20.5m;
-
+decimal total = valor3 + valor4;
 Console.WriteLine($"A soma dos decimais e: {valor3 + valor4}");
+
+double valor5 = 10.5;
+double valor6 = 20.5;
+
+Console.WriteLine($"A subtracao dos decimais e: {valor5 - valor6}");
+
+bool valor7 = false;
+
+
+if(total > 20)
+{
+    valor7 = true;
+}
+
+if(valor7)
+    Console.WriteLine("O valor e maior que 20");
+
+byte valor8 = 10;
+Console.WriteLine($"O valor e: {valor8}");
+
+short quantidade = 9;
+Console.WriteLine(quantidade);
+
+long valor9 = 1000000000000000000;
+Console.WriteLine(valor9);
+
+//Constantes Aula04///////////////////////////////////////
+
+const string nomeDoCelular ="Iphone";
+Console.WriteLine(nomeDoCelular);
+
+var casa = new Casa();
+class Casa{
+
+    }
+
