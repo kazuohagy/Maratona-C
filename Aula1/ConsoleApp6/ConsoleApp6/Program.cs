5983aaef9442b1d8e039aec61bd5f3a9791e0322
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-27 03:14:24 -0300

    switch case test

---
+++ Aula1/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Informe um mes valido entre 1 e 12");
+var mes = int.Parse(Console.ReadLine());
+
+switch (mes)
+{
+
+    case 1:
+        Console.WriteLine("Janeiro");
+        break;
+    case 2:
+        Console.WriteLine("Fevereiro");
+        break;
+    case 3:
+        Console.WriteLine("Marco");
+        break;
+    case 4:
+        Console.WriteLine("Abril");
+        break;
+    case 5:
+        Console.WriteLine("Maio");
+        break;
+    case 6:
+        Console.WriteLine("Junho");
+        break;
+    case 7:
+        Console.WriteLine("Julho");
+        break;
+    case 8:
+        Console.WriteLine("Agosto");
+        break;
+    case 9:
+        Console.WriteLine("Setembro");
+        break;
+    case 10:
+        Console.WriteLine("Outubro");
+        break;
+    case 11:
+        Console.WriteLine("Novembro");
+        break;
+    case 12:
+        Console.WriteLine("Dezembro");
+        break;
+
+
+    default:
+        Console.WriteLine("Nao encontrado");
+        break;
+}
