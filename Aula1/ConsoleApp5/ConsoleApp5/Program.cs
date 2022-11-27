38804114129d5e393f3bb7014d1d4a8df5128619
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-27 02:52:12 -0300

    :rocket: logica with ternario

--- Aula1/ConsoleApp5/ConsoleApp5/Program.cs
+++ Aula1/ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,21 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int valor1 = 10;
-int valor2 = 20;
+﻿//Calcular a media dos alunos baseado em 4 nbotas informadas e mostrar se aprovado ou reprovado
+//Ao final classificar a media 6
+//Excelente 8.5 ou mais aprovado
+//Otimo 7.5 e 8.5 ou mais aprovado
+//Boa 6 e 7.5 ou mais aprovado
+//Regular 4 e 6 
+//Ruim 4 ou menos reprovado
+using System.Reflection;
 
-if(valor1 > valor2)
+double soma = 0;
+bool aprovado=false;
+string classificacao="";
+for (int i = 0; i < 4; i++)
 {
-    Console.WriteLine("Valor 1 é maior que valor 2");
+    Console.WriteLine("Digite a nota do aluno");
+    double nota = double.Parse(Console.ReadLine());
+    soma += nota;
 }
-else if (valor1 < valor2)
+double media = soma / 4;
+if(media >= 6)
 {
-    Console.WriteLine("Valor 1 é menor que valor 2");
+    aprovado= true;
 }
 else
 {
-    Console.WriteLine("Valor 1 é igual a valor 2");
+    aprovado= false;
 }
-//condicao ternaria
-string nome = "Gustavo";
-nome = nome == "Gustavo" ? "Gustavo Kazuo" : "Outro nome";
-Console.WriteLine(nome);
+if(media>= 8.5)
+{
+    classificacao = "Excelente";
+}
+if(media< 8.5 && media >= 7.5)
+{
+    classificacao = "Otimo";
+}if(media< 7.5 && media >= 6)
+{
+    classificacao = "Boa";
+}if(media< 6 && media >= 4)
+{
+    classificacao = "Regular";
+}
+if(media< 4)
+{
+    classificacao = "Ruim";
+}
+
+string resultado = aprovado? $"Aluno e {classificacao} e foi aprovado" : $"Aluno e {classificacao} e foi reprovado";
+Console.WriteLine("Media do Aluno: " + media);
+Console.WriteLine(resultado);
