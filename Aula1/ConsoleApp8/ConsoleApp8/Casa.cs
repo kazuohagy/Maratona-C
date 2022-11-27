e137cbfc7827d2be4f8b1284a4a7cba148991e73
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-27 19:29:56 -0300

    Orientacao a objeto

---
+++ Aula1/ConsoleApp8/ConsoleApp8/Casa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Casa
+    {
+        public int Tamanho { get; set; }
+        public string Cor { get; set; }
+
+        public int QuantidadeDePortas { get; set; }
+        public int QuantidadeDeJanelas { get; set; }
+
+    }
+}
---
+++ Aula1/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp8;
+
+Console.WriteLine("CASA INSTANCIADA OBJETO!");
+var casa = new Casa();
+casa.Cor = "Laranja";
+casa.QuantidadeDePortas = 1;
+casa.QuantidadeDeJanelas= 2;
+casa.Tamanho = 500;
+
+Console.WriteLine("A cor da casa e " + casa.Cor);
+Console.WriteLine("A quantidade de portas e " + casa.QuantidadeDePortas);
+
+var casa2 = new Casa();
+casa2.Cor = "Vermelho";
+casa2.QuantidadeDePortas = 2;
+
+Console.WriteLine("A cor da casa e " + casa2.Cor);
+Console.WriteLine("A quantidade de portas e " + casa2.QuantidadeDePortas);
